706997d72e05526fe8039948c975bfc14c97bb27
Mike Adeleke <oikeoluwa@gmail.com>
2025-11-17 13:12:08 +0100

    removed schedule consultation

--- pages/index.js
+++ pages/index.js
@@ -63,12 +63,12 @@ export default function Home() {
               <p className="text-xl md:text-2xl mb-8 max-w-3xl mx-auto">
                 Legal Strategy, Practical Solutions, Trusted Results
               </p>
-              <a
+              {/* <a
                 href="#contact"
                 className="bg-white text-black font-semibold py-3 px-8 rounded-full hover:bg-blue-100 transition duration-300"
               >
                 Schedule a Consultation
-              </a>
+              </a> */}
             </div>
           </section>
 
